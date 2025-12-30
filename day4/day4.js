d485c2a94772229c89b258685d3c4ca1115a7462
di229 <169496356+di229@users.noreply.github.com>
2025-12-30 21:32:34 +0000

    day 4 part 2

--- day4/day4.js
+++ day4/day4.js
@@ -11,34 +11,47 @@ export async function readGrid(filepath) {
   const data = await readFile(filepath);
   return new Grid(
     data.indexOf('\n'),
-    data.filter(x => x !== 0xa).toString('utf-8'));
+    data.filter(x => x !== 0xa).toString('utf-8').split(''));
 }
 
 export function day4(grid) {
   // grid is represented by an array, grid.data and a row length, grid.rowLen
+  const scores = new Array(grid.data.length);
+  scores.fill(0);
+  day4inner(grid.data, grid.rowLen, scores);
+  // count the number of paper rolls with a score < 4
+  let accessible = 0;
+  for (let i=0; i < grid.data.length; i++) {
+    if (grid.data[i] == '@' && scores[i] < 4) {
+      accessible += 1;
+    }
+  }
+  return accessible;
+}
+
+function day4inner(gridArray, rowLen, scores) {
   // 8 adjacent positions, with relative row position:
-  const neighbours = [ 
-      [-1, -grid.rowLen - 1], [-1, -grid.rowLen] , [-1, 1 - grid.rowLen], 
+  const neighbours = [
+      [-1, -rowLen - 1], [-1, -rowLen] , [-1, 1 - rowLen],
       [0, -1] , [0, 1],
-      [1, grid.rowLen - 1], [1, grid.rowLen], [1, grid.rowLen + 1],
+      [1, rowLen - 1], [1, rowLen], [1, rowLen + 1],
   ];
-  const valid = (mid, rrow, nidx) => {  
-    // test if a given `nidx` is a valid neighbour index to a midpoint `mid`, 
+  const valid = (mid, rrow, nidx) => {
+    // test if a given `nidx` is a valid neighbour index to a midpoint `mid`,
     // given its expected relative row `rrow`.
-    if (nidx < 0 || nidx > grid.data.length) {
+    if (nidx < 0 || nidx > gridArray.length) {
       return false;
     }
-    if (Math.floor(mid / grid.rowLen) + rrow !== Math.floor(nidx / grid.rowLen)) {
+    if (Math.floor(mid / rowLen) + rrow !== Math.floor(nidx / rowLen)) {
       return false;
     }
     return true;
   }
-  const scores = new Array(grid.data.length);
-  scores.fill(0);
-  for (let i=0; i < grid.data.length; i++) {
-    // if a grid position contains a paper roll, add 1 to all of its valid 
+
+  for (let i=0; i < gridArray.length; i++) {
+    // if a grid position contains a paper roll, add 1 to all of its valid
     // neighbours' scores
-    if (grid.data[i] !== '@') {
+    if (gridArray[i] !== '@') {
       continue;
     }
     for (const n of neighbours) {
@@ -49,12 +62,25 @@ export function day4(grid) {
       }
     }
   }
-  // count the number of paper rolls with a score < 4
+}
+
+export function day4e(grid) {
+  const scores = new Array(grid.data.length);
+  scores.fill(0);
   let accessible = 0;
-  for (let i=0; i < grid.data.length; i++) {
-    if (grid.data[i] == '@' && scores[i] < 4) {
-      accessible += 1;
+  let moreRemoved = true;
+  while (moreRemoved) {
+    moreRemoved = false;
+    day4inner(grid.data, grid.rowLen, scores);
+    // count the number of paper rolls with a score < 4, remove them
+    for (let i=0; i < grid.data.length; i++) {
+      if (grid.data[i] === '@' && scores[i] < 4) {
+        grid.data[i] = '.';
+        accessible += 1;
+        moreRemoved = true;
+      }
     }
+    scores.fill(0);
   }
   return accessible;
 }
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import assert from 'node:assert/strict';
 import { readInputLines, day1, day1e } from './day1/day1.js';
 import { readInputRanges, day2 } from './day2/day2.js';
 import { day3 } from './day3/day3.js';
-import { readGrid, day4 } from './day4/day4.js';
+import { readGrid, day4, day4e } from './day4/day4.js';
 
 await test("Day 1", t => {
   t.test("part 1 sample result is 3", async () => {
@@ -26,33 +26,39 @@ await test("Day 1", t => {
 await test("Day 2", async t => {
   const sample = await readInputRanges('day2/inputs/sample');
   const input = await readInputRanges('day2/inputs/input');
-  t.test("part 1 sample result is 1227775554", () => {
+  await t.test("part 1 sample result is 1227775554", () => {
     assert.strictEqual(day2(sample), 1227775554);
   });
 
-  t.test("part 1 result is 44487518055", () => {
+  await t.test("part 1 result is 44487518055", () => {
     assert.strictEqual(day2(input), 44487518055);
   });
 });
 
 await test("Day 3", async t => {
   const sample = await readInputLines('day3/inputs/sample');
-  t.test("part 1 sample result is 357", () => {
+  await t.test("part 1 sample result is 357", () => {
     assert.strictEqual(day3(sample), 357);
   });
   const input = await readInputLines('day3/inputs/input');
-  t.test("part 1 result is 16887", () => {
+  await t.test("part 1 result is 16887", () => {
     assert.strictEqual(day3(input), 16887);
   });
 });
 
 await test("Day 4", async t => {
   const sampleGrid = await readGrid('day4/inputs/sample');
-  t.test("part 1 sample result is 13", () => {
+  await t.test("part 1 sample result is 13", () => {
     assert.strictEqual(day4(sampleGrid), 13);
   });
   const inputGrid = await readGrid('day4/inputs/input');
-  t.test("part 1 result is 1547", () => {
+  await t.test("part 1 result is 1547", () => {
     assert.strictEqual(day4(inputGrid), 1547);
+  });
+  await t.test("part 2 sample result is 43", () => {
+    assert.strictEqual(day4e(sampleGrid), 43);
+  });
+  await t.test("part 2 result is 43", () => {
+    assert.strictEqual(day4e(inputGrid), 43);
   });
 });
